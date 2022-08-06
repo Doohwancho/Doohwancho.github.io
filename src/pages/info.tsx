b44477e4695a6889675711572c40e3d3d013c1ed
Doohwancho <doohwancho526@gmail.com>
2022-08-06 19:36:23 +0900

    playground: apply styled component with props

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,7 +1,34 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
+import { Global, css } from '@Emotion/react' 
+import styled from '@Emotion/styled'
+
+const globalStyle = css`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        
+        font-size: 20px;
+     }
+`
+
+const TextStyle = css`
+    font-size: 18px;
+    font-weight: 700;
+    color: gray;
+`
+
+const Text1 = styled.div<{ disable: boolean }>`
+    font-size: 20px;
+    font-weight: 700;
+    text-decoration: ${({disable}) => (disable ? 'line-through' : 'none')};
+`
+
+const Text2 = styled('div')<{ disable: boolean }>(() => ({
+    contSize: '15px',
+    color: 'blue'
+}))
 
 type InfoPageProps = {
     data: {
@@ -25,10 +52,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
     }) {   
 	return (
 		<div>
-			<Text text={title} />
-			<Text text={description} />
-			<Text text={author} />
-            <Link to="/">To Main</Link>
+            <Global styles={globalStyle} />
+            <div css={TextStyle}>{title}</div> 
+            <Text1 disable={true}>{description}</Text1>
+            <Text2 disable={true}>{author}</Text2> 
 		</div>
 	)
 }
