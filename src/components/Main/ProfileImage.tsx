1e607f8a873c492ba9b3da08cdc23e890ffb6bc1
Doohwancho <doohwancho526@gmail.com>
2022-08-06 20:09:28 +0900

    playground: main page header and footer

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@Emotion/styled'
+
+const PROFILE_IMAGE_LINK =`https://i.pinimg.com/originals/25/78/61/25786134576ce0344893b33a051160b1.jpg`; 
+
+const ProfileImageWrapper = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+        return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+    }
+
+export default ProfileImage
