1afec2956ed49905a4266716ebf77f698a8ca6b3
Doohwancho <doohwancho526@gmail.com>
2022-08-10 15:43:23 +0900

    bare html with no style on post

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { PostPageItemType } from 'types/PostItem.types'  // 바로 아래에서 정의할 것입니다
 import Template from 'components/Common/Template' 
 import PostHead from 'components/Post/PostHead' 
+import PostContent from 'components/Post/PostContent'
 
 type PostTemplateProps = {
   data: {
@@ -30,7 +31,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
         },
       },
     },
-  } = edges[ 0]
+  } = edges[0]
 
   return (
     <Template>
@@ -40,6 +41,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
         categories={categories}
         thumbnail={gatsbyImageData}
       />
+      <PostContent html={html} />
     </Template>
   )
 }
