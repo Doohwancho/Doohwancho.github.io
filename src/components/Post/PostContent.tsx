e1cf2ecba8ba49a4ab841c2f284126c8c500790c
Doohwancho <doohwancho526@gmail.com>
2022-08-10 16:06:56 +0900

    html style on post re-adjust

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -84,23 +84,50 @@ const MarkdownRenderer = styled.div`
   }
 
   // Adjust Code Style
-  pre[class*='language-'] {
+  pre[class*='language-']  {
     margin: 30px 0;
     padding: 15px;
     font-size: 15px;
 
-    ::-webkit-scrollbar-thumb {
+    ::-webkit-scrollbar-thumb  {
       background: rgba(255, 255, 255, 0.5);
       border-radius: 3px;
     }
   }
 
-  code[class*='language-'],
-  pre[class*='language-'] {
+  code[class*='language-'], 
+  pre[class*='language-']  {
     tab-size: 2;
   }
-`
 
+  // Markdown Responsive Design
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 80px 20px;
+    line-height: 1.6;
+    font-size: 14px;
+
+    h1 {
+      font-size: 23px;
+    }
+
+    h2 {
+      font-size: 20px;
+    }
+
+    h3 {
+      font-size: 17px;
+    }
+
+    img {
+      width: 100%;
+    }
+
+    hr {
+      margin: 50px 0;
+    }
+  }
+`
 
 const PostContent: FunctionComponent<PostContentProps> = function ({ html }) {
   return <MarkdownRenderer dangerouslySetInnerHTML={{ __html: html }} />
